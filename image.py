44813b395c55808510164d696b8264f6eb58a5e0
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2021-12-31 12:52:13 -0700

    imagenes radiobutton manejo de ventanas

--- image.py
+++ image.py
@@ -2,13 +2,60 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.title('imagenes')
-
+root.title('Carrusel')
 #imagen = Image.open('imgp.png')
 #imagen.show()
+img1 = ImageTk.PhotoImage(Image.open('images/1.png'))
+img2 = ImageTk.PhotoImage(Image.open('images/2.png'))
+img3 = ImageTk.PhotoImage(Image.open('images/3.png'))
+img4 = ImageTk.PhotoImage(Image.open('images/4.png'))
+list = [img1, img2, img3, img4]
 
-img = ImageTk.PhotoImage(Image.open('imgp.png'))
-l = Label(image=img)
-l.pack()
+l = Label(root, image=img1)
+l.grid(row=0, column=0, columnspan=3)
+
+def adelante(img_num):
+    global l
+    global btn_atras
+    global btn_adelante
+
+    l.grid_forget()
+    btn_atras.grid_forget()
+    l = Label(root, image=list[img_num])
+    btn_atras = Button(root, text='<-', command= lambda: atras(img_num - 1))
+    btn_adelante = Button(root, text='->', command=lambda: adelante(img_num + 1))
+
+    if img_num == 3:
+        btn_adelante = Button(root, text='N/A', state=DISABLED)
+
+    l.grid(row=0, column=0, columnspan=3)
+    btn_atras.grid(row=1, column=0)
+    btn_adelante.grid(row=1, column=2)
+
+
+def atras(img_num):
+    global l
+    global btn_atras
+    global btn_adelante
+
+    l.grid_forget()
+    btn_adelante.grid_forget()
+    l = Label(root, image=list[img_num])
+    btn_atras = Button(root, text='<-', command= lambda: atras(img_num - 1))
+    btn_adelante = Button(root, text='->', command=lambda: adelante(img_num + 1))
+
+    if img_num == 0:
+        btn_atras = Button(root, text='N/A', state=DISABLED)
+
+    l.grid(row=0, column=0, columnspan=3)
+    btn_atras.grid(row=1, column=0)
+    btn_adelante.grid(row=1, column=2)
+
+
+btn_atras = Button(root, text='N/A', state=DISABLED)
+btn_adelante = Button(root, text='->', command=lambda: adelante(1))
+
+btn_atras.grid(row=1, column=0)
+btn_adelante.grid(row=1, column=2)
 
 root.mainloop()
